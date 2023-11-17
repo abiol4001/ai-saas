ae9dde9cd54a73d75d23b90a1cc33795366faa82
Abiola Oyewale <abiol4001@gmail.com>
2023-11-17 11:37:48 +0100

    landing page fixed

---
+++ src/app/(landing)/page.tsx
@@ -0,0 +1,17 @@
+import { LandingHero } from '@/components/LandingHero'
+import { LandingNavbar } from '@/components/LandingNavbar'
+import React from 'react'
+
+type Props = {}
+
+const page = (props: Props) => {
+  return (
+      <div className="h-[100vh] bg-[#111827]">
+          <LandingNavbar />
+          <LandingHero />
+          {/* <LandingContent /> */}
+      </div>
+  )
+}
+
+export default page
